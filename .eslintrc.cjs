ccde7a25d80202a519b116791eca58474d756490
Thiago Martins <thiago_valentim1@hotmail.com>
2023-12-01 09:46:52 -0300

    style: configure prettier for workspace

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,21 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports', // Enforce type-imports wherever possible
+        disallowTypeAnnotations: false, // Allows type annotations
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+  },
 };
