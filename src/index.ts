1b9786a750417065b649fdff1390969d76a1fa19
Rick Dutour Geerling <rick@trilon.io>
2024-01-12 10:08:54 +0100

    chore: prepare for first publish
    Tested package with yalc

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import enforceCloseTestingModuleRule from './rules/enforce-close-testing-module.
 
 // TODO: we should type this as ESLint.Plugin but there's a type incompatibilities with the utils package
 const plugin = {
+  configs: {
+    recommended: {
+      rules: { '@trilon/enforce-close-testing-module': 'error' },
+    },
+  },
   rules: {
-    '@trilon/enforce-close-testing-module': enforceCloseTestingModuleRule,
+    'enforce-close-testing-module': enforceCloseTestingModuleRule,
   },
 };
 
