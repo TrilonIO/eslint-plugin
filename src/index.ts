5b5a707968c958235b42a9115fa485aba2236e97
Thiago Martins <thiago_valentim1@hotmail.com>
2024-01-25 09:17:33 -0300

    chore: update detect circular reference docs and add it to recommended rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import enforceCloseTestingModuleRule from './rules/enforce-close-testing-module.rule';
 import checkInjectDecoratorRule from './rules/check-inject-decorator.rule';
+import detectCircularReferenceRule from './rules/detect-circular-reference.rule';
 // TODO: we should type this as ESLint.Plugin but there's a type incompatibilities with the utils package
 const plugin = {
   configs: {
@@ -7,12 +8,14 @@ const plugin = {
       rules: {
         '@trilon/enforce-close-testing-module': 'error',
         '@trilon/check-inject-decorator': 'error',
+        '@trilon/detect-circular-reference': 'warn',
       },
     },
   },
   rules: {
     'enforce-close-testing-module': enforceCloseTestingModuleRule,
     'check-inject-decorator': checkInjectDecoratorRule,
+    'detect-circular-reference': detectCircularReferenceRule,
   },
 };
 
